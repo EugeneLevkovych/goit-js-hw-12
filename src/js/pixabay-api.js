6e6da51590906a06a50a0195f016662c62e24ad4
EugeneLevkovych <eugenelevkovych@yahoo.com>
2025-01-22 18:33:00 +0200

    add async

--- src/js/pixabay-api.js
+++ src/js/pixabay-api.js
@@ -1,9 +1,5 @@
 import axios from 'axios';
 
-// const myApiKey = '48247224-415eb498da8da81883dddb739';
-
-// axios.defaults.headers.common['key'] = myApiKey;
-
 export const fetchPhotosByQuery = searchedQuery => {
   const searchParams = new URLSearchParams({
     key: '48247224-415eb498da8da81883dddb739',
@@ -11,6 +7,7 @@ export const fetchPhotosByQuery = searchedQuery => {
     image_type: 'photo',
     orientation: 'horizontal',
     safesearch: true,
+    per_page: 15,
   });
 
   return axios.get(`https://pixabay.com/api/?${searchParams}`);
